a82a79d343551f7824b10ba50046713adf269939
Oleksandra Fedorova <emoilenne@gmail.com>
2017-12-15 16:36:13 +0100

    rename class to Posts

--- app/javascript/components/Posts.js
+++ app/javascript/components/Posts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class SampleComponent extends React.Component {
+export default class Posts extends React.Component {
   render() {
     return (
       <h3>Latest posts</h3>
