bdf63ff66c7dca1bb30e7f9b00b892b626146a97
Oleksandra Fedorova <emoilenne@gmail.com>
2017-12-22 15:55:56 +0100

    added comments_wrapper

---
+++ app/javascript/packs/components/comments_wrapper.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import NewComment from './new_comment';
+import AllComments from './all_comments';
+
+export default class CommentsWrapper extends Component {
+  constructor() {
+    super();
+    this.state = { comments: [] };
+  }
+
+  componentDidMount() {
+    $.getJSON(`/api/posts/${this.props.post_id}/comments`, (response) => { this.setState({comments: response}); });
+  }
+
+  updateComments = (comment) => {
+    var comments = this.state.comments.filter((c) => {
+      return c.id != comment.id;
+    });
+    comments.push(comment);
+    this.setState({ comments: comments })
+  }
+
+  removeComment = (id) => {
+    var newComments = this.state.comments.filter((comment) => {
+      return comment.id != id;
+    });
+    this.setState({ comments: newComments });
+  }
+
+  handleCommentUpdate = (comment) => {
+    $.ajax({
+      url: `/api/comments/${comment.id}`,
+      type: 'PUT',
+      data: { comment: comment },
+      success: (comment) => {
+        this.updateComments(comment);
+      }
+    });
+  }
+
+  handleCommentDelete = (id) => {
+    $.ajax({
+      url: `/api/comments/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeComment(id);
+      }
+    });
+  }
+
+  render() {
+      return (
+        <div className="comment-section">
+          <AllComments
+            comments={this.state.comments}
+            handleDelete={this.handleCommentDelete}
+            handleUpdate={this.handleCommentUpdate}
+            post_id={this.props.post_id}
+            userExistsOrCreate={this.props.userExistsOrCreate}/>
+          <NewComment
+            handleSubmit={this.updateComments}
+            post_id={this.props.post_id}
+            userExistsOrCreate={this.props.userExistsOrCreate}/>
+        </div>
+      );
+    }
+}
--- app/javascript/packs/components/post.js
+++ app/javascript/packs/components/post.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import NewComment from './new_comment';
-import AllComments from './all_comments';
+import CommentsWrapper from './comments_wrapper'
 
 export default class Post extends Component {
   constructor() {
     super();
-    this.state = { editable: false, comments: [], username: "" };
+    this.state = { editable: false, username: "" };
   }
 
   componentDidMount() {
-    $.getJSON(`/api/posts/${this.props.post.id}/comments`, (response) => { this.setState({comments: response}); });
     $.getJSON(`/api/users/${this.props.post.user_id}`, (response) => { this.setState({username: response.name}); });
   }
 
@@ -27,42 +25,6 @@ export default class Post extends Component {
     else {
       this.setState({ editable: true })
     }
-  }
-
-  updateComments = (comment) => {
-    var comments = this.state.comments.filter((c) => {
-      return c.id != comment.id;
-    });
-    comments.push(comment);
-    this.setState({ comments: comments })
-  }
-
-  removeComment = (id) => {
-    var newComments = this.state.comments.filter((comment) => {
-      return comment.id != id;
-    });
-    this.setState({ comments: newComments });
-  }
-
-  handleCommentUpdate = (comment) => {
-    $.ajax({
-      url: `/api/comments/${comment.id}`,
-      type: 'PUT',
-      data: { comment: comment },
-      success: (comment) => {
-        this.updateComments(comment);
-      }
-    });
-  }
-
-  handleCommentDelete = (id) => {
-    $.ajax({
-      url: `/api/comments/${id}`,
-      type: 'DELETE',
-      success: () => {
-        this.removeComment(id);
-      }
-    });
   }
 
   render() {
@@ -83,18 +45,9 @@ export default class Post extends Component {
             </div>
             <div>{body}</div>
           </div>
-          <div className="comment-section">
-            <AllComments
-              comments={this.state.comments}
-              handleDelete={this.handleCommentDelete}
-              handleUpdate={this.handleCommentUpdate}
+            <CommentsWrapper
               post_id={this.props.post.id}
               userExistsOrCreate={this.props.userExistsOrCreate}/>
-            <NewComment
-              handleSubmit={this.updateComments}
-              post_id={this.props.post.id}
-              userExistsOrCreate={this.props.userExistsOrCreate}/>
-          </div>
         </div>
       );
     }
