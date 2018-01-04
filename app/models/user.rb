0e737d7e3642b0aabb1520d19ed1358b7c6d0689
Oleksandra Fedorova <emoilenne@gmail.com>
2018-01-04 13:34:13 +0100

    validate username only with letters and numbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  validates :name, presence: true
+  validates :name, presence: true, format: { with: /\A[a-zA-Z0-9]*\z/, message: 'can\'t have special characters, only letters and numbers' }
   validates_uniqueness_of :name
 end
